8d3f6eb7e6299c6903b27f74aa435b1349088096
Lawrence <lchong2@ucsc.edu>
2017-01-18 04:22:30 -0800

    Added MPAndroidChart library and imported LineChart

--- app/src/main/java/sdp/barbelltrainer/MainActivity.java
+++ app/src/main/java/sdp/barbelltrainer/MainActivity.java
@@ -2,8 +2,11 @@ package sdp.barbelltrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.github.mikephil.charting.charts.LineChart;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LineChart linechart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
