b9be4a173b83987a8bae5e22b0538d795b7b2815
lchong2 <lchong2@ucsc.edu>
2017-01-28 12:50:35 -0800

    Added start button to graph inside New activity

--- app/src/main/java/sdp/barbelltrainer/NewActivity.java
+++ app/src/main/java/sdp/barbelltrainer/NewActivity.java
@@ -1,5 +1,6 @@
 package sdp.barbelltrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // New import files
@@ -12,6 +13,8 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,42 +23,57 @@ import static com.github.mikephil.charting.components.XAxis.XAxisPosition.BOTH_S
 
 public class NewActivity extends AppCompatActivity {
 
+    private Button start_new_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new2);
-
+    // LineChart vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         // LineChart object
         LineChart linechart = (LineChart) findViewById((R.id.linechart));
 
         // x-axis
         ArrayList<String> x_axis = new ArrayList<>();
-        for (double i = -1.0;i<=1.0;i+=1){
-            x_axis.add(String.valueOf(i));
+        for (double i = -1.0f;i<=1.0;i+=0.10){
+            x_axis.add(String.valueOf(Math.round(i*10)/10.0));
         }
 
         // List of Entries
         float[] data = {0};
         ArrayList<Entry> entries = new ArrayList<Entry>();
 
-        for (float f:data){
-            entries.add(new Entry(f, 1));
-        }
+        entries.add(new Entry(0, 10));
 
         //LineDataSet
-        LineDataSet dataset = new LineDataSet(entries, "bar path");
-        dataset.setColor(Color.RED);
+        LineDataSet linedataset = new LineDataSet(entries, "bar path");
+        linedataset.setColor(Color.RED);
 
         //List of ILineDataSet
-        ArrayList<ILineDataSet> Set = new ArrayList<>();
-        Set.add(dataset);
+        ArrayList<ILineDataSet> set = new ArrayList<>();
+        set.add(linedataset);
 
         //LineData
-        LineData linedata = new LineData(x_axis, Set);
+        LineData linedata = new LineData(x_axis, set);
         linechart.setData(linedata);
         linechart.invalidate();
 
         //Styling linechart
         linechart.setDescription("");
+    // LineChart ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    // Start Button vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        // Listener for "Start" button in NewActivity
+        start_new_button = (Button)findViewById(R.id.start_new_button);
+        start_new_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (start_new_button.getText().toString().compareTo("Start") == 0) {
+                    start_new_button.setText("Stop");
+                }
+                else start_new_button.setText("Start");
+            }
+        });
+    // Start Button ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     }
 }
