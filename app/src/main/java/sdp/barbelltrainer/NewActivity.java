8b484def020c0f449f3e09ac57ef3a42f2b3b563
lchong2 <lchong2@ucsc.edu>
2017-02-05 20:47:46 -0800

    Refined graph, mapped xy accelerometer values to graph and printed to screen

--- app/src/main/java/sdp/barbelltrainer/NewActivity.java
+++ app/src/main/java/sdp/barbelltrainer/NewActivity.java
@@ -1,7 +1,12 @@
 package sdp.barbelltrainer;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // New import files
@@ -29,18 +34,49 @@ import static com.github.mikephil.charting.components.XAxis.XAxisPosition.BOTH_S
 import static java.lang.Thread.sleep;
 
 
-public class NewActivity extends AppCompatActivity {
+public class NewActivity extends AppCompatActivity implements SensorEventListener {
+
+    int cursor_x = 1000;
+    float cursor_y = 0;
+
+    private SensorManager sensorManager;
+    private Sensor accel;
+    float ax, ay, az;
 
     static ExecutorService threadpool = Executors.newFixedThreadPool(2);
-    static int i = 0;
+
     private Button start_new_button;
     static boolean recording;
+
     static LineChart linechart;
     static ArrayList<String> x_axis;
     static ArrayList<Entry> entries;
     static LineDataSet linedataset;
     static LineData linedata;
-    Context lola = this;
+// Accelerometer functions vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            ax = event.values[0];
+            ay = event.values[1];
+            az = event.values[2];
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+
+    }
+// ---------------------------------------------------------------------------
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -53,15 +89,15 @@ public class NewActivity extends AppCompatActivity {
 
         // x-axis
         x_axis = new ArrayList<>();
-        for (double i = -1.0f;i<=1.0;i+=0.10){
-            x_axis.add(String.valueOf(Math.round(i*10)/10.0));
+        for (double i = -1.0f;i<=1.0;i+=0.001){
+            x_axis.add(String.valueOf(Math.round(i*1000)/1000.0));
         }
 
         // List of Entries
         float[] data = {0};
         entries = new ArrayList<Entry>();
 
-        entries.add(new Entry(0, 10));
+        entries.add(new Entry(cursor_y, cursor_x));
 
         //LineDataSet
         linedataset = new LineDataSet(entries, "bar path");
@@ -77,8 +113,25 @@ public class NewActivity extends AppCompatActivity {
         linechart.invalidate();
 
         //Styling linechart
+        linedataset.setDrawValues(false);
+        linedataset.setHighlightEnabled(false);
+        linedataset.setDrawCircles(false);
         linechart.setDescription("");
+        linechart.setPinchZoom(true);
+        linechart.setDragEnabled(true);
+        linechart.setScaleEnabled(true);
+        linechart.setTouchEnabled(true);
     // LineChart ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    // Initializing accelerometer vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+        final TextView xtext = (TextView) findViewById(R.id.ax);
+        final TextView ytext = (TextView) findViewById(R.id.ay);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
     // Start Button vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         // Listener for "Start" button in NewActivity
@@ -90,19 +143,20 @@ public class NewActivity extends AppCompatActivity {
                     recording = true;
                     start_new_button.setText("Stop");
 
-
                     Future future = threadpool.submit(new Runnable()  {
                         public void run() {
                             while (recording) {
-
-                                linedataset.addEntry(new Entry((float)Math.round(Math.random()*10), (int)Math.round(Math.random()*20)));
+                                cursor_x+=Math.round(ax);
+                                cursor_y+=ay/1000.0;
+                                linedataset.addEntry(new Entry(cursor_y,cursor_x));
                                 linechart.notifyDataSetChanged();
-
 
 
                                 runOnUiThread(new Runnable() {
                                     public void run() {
                                         linechart.invalidate();
+                                        xtext.setText("x-value:" + Float.toString((Math.round(ax))));
+                                        ytext.setText("y-value:" + Float.toString((float)(ay/1000.0)));
                                     }
                                 });
                                 try {
