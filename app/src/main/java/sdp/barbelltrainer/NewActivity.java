79acf8470e49a017423fb8c3ee6dd338b662d7ac
lchong2 <lchong2@ucsc.edu>
2017-04-12 14:48:38 -0700

    Added gyroscope and calibration orientation values

--- app/src/main/java/sdp/barbelltrainer/NewActivity.java
+++ app/src/main/java/sdp/barbelltrainer/NewActivity.java
@@ -11,6 +11,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // New import files
+import java.lang.Math;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -32,6 +33,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 import static com.github.mikephil.charting.components.XAxis.XAxisPosition.BOTH_SIDED;
+import static java.lang.Math.atan2;
 import static java.lang.Thread.sleep;
 
 
@@ -39,13 +41,16 @@ public class NewActivity extends AppCompatActivity implements SensorEventListene
 
     int cursor_x = 1000;
     float cursor_y = 0;
+    float theta = 0;
 
     float vel_x = 0;
     float vel_y = 0;
 
     private SensorManager sensorManager;
     private Sensor accel;
+    private Sensor gyro;
     float ax, ay, az;
+    float gx, gy, gz;
 
     static ExecutorService threadpool = Executors.newFixedThreadPool(2);
 
@@ -75,13 +80,19 @@ public class NewActivity extends AppCompatActivity implements SensorEventListene
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyro, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             ax = event.values[0];
             ay = event.values[1];
             az = event.values[2];
+        }
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE_UNCALIBRATED) {
+            gx = event.values[0];
+            gy = event.values[1];
+            gz = event.values[2];
         }
     }
 
@@ -96,6 +107,7 @@ public class NewActivity extends AppCompatActivity implements SensorEventListene
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new2);
+
     // LineChart vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         // LineChart object
         linechart = (LineChart) findViewById((R.id.linechart));
@@ -142,9 +154,12 @@ public class NewActivity extends AppCompatActivity implements SensorEventListene
         final TextView ytext = (TextView) findViewById(R.id.ay);
         final TextView ztext = (TextView) findViewById(R.id.az);
         final TextView sensor_v = (TextView) findViewById(R.id.sv);
+        final TextView theta_v = (TextView) findViewById(R.id.tv);
+        final TextView theta_v2 = (TextView) findViewById(R.id.tv2);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        accel = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
         sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
 
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -158,6 +173,7 @@ public class NewActivity extends AppCompatActivity implements SensorEventListene
                 if(start_new_button.getText().toString().compareTo("Start") == 0){
                     recording = true;
                     start_new_button.setText("Stop");
+                    theta = (float)(atan2(ax, ay*-1)*(180/Math.PI)+180);
 
                     Future future = threadpool.submit(new Runnable()  {
                         public void run() {
@@ -168,19 +184,26 @@ public class NewActivity extends AppCompatActivity implements SensorEventListene
                                 vel_y = vel_y/2.0f + Math.round((Math.round(ay*10)/10.0))/100.0f;
                                 cursor_y = cursor_y + vel_y/100.0f;
 
+
+
                                 //cursor_x+=Math.round(ax*10)/10.0;
                                 //cursor_y+=(Math.round(ay*10)/10.0)/1000.0;
                                 linedataset.addEntry(new Entry(cursor_y,cursor_x));
                                 linechart.notifyDataSetChanged();
 
-
-
+                                theta-=gz*0.01*(180/Math.PI);
+                                if (Math.round(theta) == -1) {
+                                    theta = 359;
+                                }
+                                theta = theta % 360;
                                 runOnUiThread(new Runnable() {
                                     public void run() {
                                         linechart.invalidate();
                                         xtext.setText("x-value:" + Float.toString(ax));
                                         ytext.setText("y-value:" + Float.toString((float)(ay)));
                                         ztext.setText("z-value:" + Float.toString((float)(az)));
+                                        theta_v.setText("theta(real):" + Float.toString((float)(atan2(ax, ay*-1)*(180/Math.PI)+180)));
+                                        theta_v2.setText("theta(est):" + theta);
                                         sensor_v.setText("sensor:" + DeviceControlActivity.sensor_value);
                                     }
                                 });
